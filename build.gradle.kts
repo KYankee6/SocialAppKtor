2882cecd3b0511f96a143c0cfde0d5887976760e
uicheon <highestbright98@naver.com>
2025-01-24 01:04:12 +0900

    fix: downgrade ktor-koin for compatibility problem :(

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val h2_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val postgres_version: String by project
-
+val ktor_version: String by project
 val koin_version: String by project
 val hikaricp_version: String by project
 
@@ -31,8 +31,8 @@ dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-auth")
     implementation("io.ktor:ktor-server-auth-jwt")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
